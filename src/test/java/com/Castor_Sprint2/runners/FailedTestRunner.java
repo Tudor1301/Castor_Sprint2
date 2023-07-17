006bb821454fc7411e8057e1ad96efed70fefbe2
ivandelibasic <ivanr.delibasic@gmail.com>
2023-07-17 11:19:51 -0400

    Initial commit

---
+++ src/test/java/com/Castor_Sprint2/runners/FailedTestRunner.java
@@ -0,0 +1,15 @@
+package com.Castor_Sprint2.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "@target/rerun.txt",
+        glue = "com/Castor_Sprint2/step_definitions"
+)
+public class FailedTestRunner {
+}
+
+
